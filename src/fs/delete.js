b539c3acf64235757fd97b6e296946da53d74da8
UlianaSavi <uljasha.sav@gmail.com>
2023-06-06 17:48:03 +0600

    feat: implemetn delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,14 @@
+import {
+    promises as fs
+} from 'fs';
+
+const path = './src/fs/files/fileToRemove.txt';
+
 const remove = async () => {
-    // Write your code here 
+    fs.rm(path).catch((err) => {
+        throw new Error(err);
+    });
+    console.log('File deleted!')
 };
 
 await remove();
