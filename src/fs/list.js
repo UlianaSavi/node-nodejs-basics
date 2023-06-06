62938e13014fcad0ec5f1b84ebc2ce55cabc5f97
UlianaSavi <uljasha.sav@gmail.com>
2023-06-06 17:54:16 +0600

    feat: implemetn list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,18 @@
+import {
+    promises as fs
+} from 'fs';
+
+const folderPath = './src/fs/files';
+
 const list = async () => {
-    // Write your code here 
+    const listOfFiles = await fs.readdir(folderPath)
+        .catch(() => {
+            throw new Error(`Can't find folder! FS operation failed.`);
+        });
+        listOfFiles.map((file, idx) => {
+            console.log(`${idx+1} -------> ${file}`);
+        })
+    return listOfFiles;
 };
 
 await list();
