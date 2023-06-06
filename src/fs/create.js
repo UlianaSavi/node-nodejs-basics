ee79fa75d6406c3bc7e8ae60c0b0ca2c9fba4f0d
UlianaSavi <uljasha.sav@gmail.com>
2023-06-06 16:46:27 +0600

    feat: implement create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,24 @@
+import { promises as fs } from 'fs';
+
+const filePath = './src/fs/files/fresh.txt';
+
+const check = async () => {
+    const res = fs.access(filePath, fs.constants.F_OK)
+        .then(() => true)
+        .catch(() => {
+            return false;
+        })
+    return res;
+};
+
 const create = async () => {
-    // Write your code here 
+    const exist = await check();
+    if (!exist) {
+        await fs.appendFile(filePath, 'I am fresh and young')
+        console.log('fresh.txt was created.');
+    } else {
+        throw new Error('fresh.txt already exist! FS operation failed.');
+    }
 };
 
 await create();
