92461076106d2ef769ca33c06d62c2f21d4f5405
UlianaSavi <uljasha.sav@gmail.com>
2023-06-06 17:32:25 +0600

    feat: implement copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,41 @@
+import {
+    promises as fs
+} from 'fs';
+
+const folderPath = './src/fs/files';
+const folderToCopyPath = './src/fs/files_copy';
+
+const readFolder = async () => {
+    const res = fs.readdir(folderPath)
+        .catch(() => {
+            throw new Error(`Cant find folder! FS operation failed.`);
+        });
+    return res;
+}
+
+const createCopyFolder = async () => {
+    const res = fs.mkdir(folderToCopyPath, {
+            recursive: false
+        })
+        .then(() => {
+            return true
+        })
+        .catch(() => {
+            return false;
+        })
+    return res;
+};
+
 const copy = async () => {
-    // Write your code here 
+    const filesList = await readFolder();
+    const copyFolderExist = await createCopyFolder();
+    if (copyFolderExist) {
+        filesList.map((file) => {
+            fs.copyFile(`${folderPath}/${file}`, `${folderToCopyPath}/${file}`);
+        });
+    } else {
+        throw new Error('folder copy already exist! FS operation failed.');
+    }
 };
 
 await copy();
