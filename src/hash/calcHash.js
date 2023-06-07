c12c5b4089337463bb6b9b32098e9d0c945e378f
UlianaSavi <uljasha.sav@gmail.com>
2023-06-07 17:00:20 +0600

    update: change top level await to async await

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -6,11 +6,11 @@ import { createHash } from 'crypto';
 const __dirname = new URL('.', import.meta.url).pathname.slice(1);
 const filename = path.join(__dirname, 'files/fileToCalculateHashFor.txt');
 
-const data = await fs.readFile(filename, 'utf-8');
+const calculateHash = async () => {
+    const data = await fs.readFile(filename, 'utf-8');
 
-const calculateHash = () => {
     sha256(data);
     const encoding = createHash('sha256').update(data).digest('hex');
     console.log(encoding);
 };
-calculateHash();
+await calculateHash();
