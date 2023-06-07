2fb2c145934cb9542d870b5f11577feb5a2fd0c2
UlianaSavi <uljasha.sav@gmail.com>
2023-06-07 14:53:10 +0600

    feat: update env.js, impove

--- src/cli/env.js
+++ src/cli/env.js
@@ -2,9 +2,14 @@ const prefix = /RSS_/;
 
 const parseEnv = () => {
     const variables = { ...process.env };
-    const list = Object.keys(variables).filter((variable) => variable.match(prefix) !== null);
-    console.log(list);
-    return list;
+    const listOfNames = Object.keys(variables).filter((variable) => variable.match(prefix));
+    const listOfValues = listOfNames.map((item) => variables[item]);
+    const result = [];
+    for (let i = 0; i < listOfNames.length; i++) {
+        result.push(`${listOfNames[i]}=${listOfValues[i]}`);
+    }
+    result.map((item) => console.log(`${item}\n-----------`))
+    return result;
 };
 
 parseEnv();
