dd039118909031c2299082bf9f76cfb7d025cca3
UlianaSavi <uljasha.sav@gmail.com>
2023-06-07 15:20:10 +0600

    feat: implement args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,19 @@
+const prefix = /--/;
+
 const parseArgs = () => {
-    // Write your code here 
+    const args = [...process.argv];
+    const listOfNames = args.filter((arg) => arg.match(prefix));
+    const listOfValues = [];
+    for (let i = 0; i < args.length; i++) {
+        args[i].match(prefix) ? listOfValues.push(args[i+1]) : 0;
+    }
+    console.log(listOfValues);
+    const result = [];
+    for (let i = 0; i < listOfNames.length; i++) {
+        result.push(`${listOfNames[i]} is ${listOfValues[i]}`);
+    }
+    result.map((item) => console.log(`${item}\n`))
+    return result;
 };
 
 parseArgs();
