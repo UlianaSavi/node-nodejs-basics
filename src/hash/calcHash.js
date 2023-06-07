d15bcde04331fd3560e59257211c09416c16458c
UlianaSavi <uljasha.sav@gmail.com>
2023-06-07 16:58:15 +0600

    feat: implement calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,16 @@
-const calculateHash = async () => {
-    // Write your code here 
-};
+import path from 'path';
+import sha256 from 'sha256';
+import { promises as fs } from 'fs';
+import { createHash } from 'crypto';
 
-await calculateHash();
+const __dirname = new URL('.', import.meta.url).pathname.slice(1);
+const filename = path.join(__dirname, 'files/fileToCalculateHashFor.txt');
+
+const data = await fs.readFile(filename, 'utf-8');
+
+const calculateHash = () => {
+    sha256(data);
+    const encoding = createHash('sha256').update(data).digest('hex');
+    console.log(encoding);
+};
+calculateHash();
