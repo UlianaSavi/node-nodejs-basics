ae141c5d40df78d22c1530d866c0009b785d820d
UlianaSavi <uljasha.sav@gmail.com>
2023-06-07 20:09:36 +0600

    feat: implement write.js in streams

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,21 @@
+import fs from 'fs';
+import path from 'path';
+
+const __dirname = new URL('.', import.meta.url).pathname.slice(1);
+const fileToWrite = path.join(__dirname, 'files/fileToWrite.txt');
+
 const write = async () => {
-    // Write your code here 
+    const writebleStream = fs.createWriteStream(fileToWrite);
+
+	process.stdin.on('readable', () => {
+		writebleStream._write(process.stdin.read(), 'utf8', (error) => {
+            if (error) {
+                console.log(error.message)
+            } else {
+                return;
+            }
+        });
+	});
 };
 
 await write();
