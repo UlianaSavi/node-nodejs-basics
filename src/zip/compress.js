d4a732e4a2a94900bc54bfc4c9b7705c68bff55e
UlianaSavi <uljasha.sav@gmail.com>
2023-06-08 15:39:42 +0600

    feat: implement compress.js in zip tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,19 @@
+import fs from 'fs';
+import path from 'path';
+import { createGzip } from 'zlib';
+
+const __dirname = new URL('.', import.meta.url).pathname.slice(1);
+const fileToCompress = path.join(__dirname, 'files/fileToCompress.txt');
+const filExtension = 'gz';
+
 const compress = async () => {
-    // Write your code here 
+    const readableStream = fs.createReadStream(fileToCompress);
+    readableStream
+        .pipe(createGzip())
+        .pipe(fs.createWriteStream(`${ fileToCompress.slice(0, fileToCompress.length - 4) }.${ filExtension }`))
+        .on('finish', () => {
+            console.log(`Compression process done: ${ fileToCompress }`)
+        })
 };
 
 await compress();
