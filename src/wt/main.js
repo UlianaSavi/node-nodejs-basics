4de795f262ab6979f4f20bbcbef42f5155bdf0c0
UlianaSavi <uljasha.sav@gmail.com>
2023-06-08 22:53:29 +0600

    feat: update worker and finish with main.js in wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,34 +1,38 @@
 import path from 'path';
 import os from 'os';
-import { Worker, MessageChannel } from 'worker_threads';
+import { Worker } from 'worker_threads';
 
 const __dirname = new URL('.', import.meta.url).pathname.slice(1);
 const pathToWorker = path.join(__dirname, './worker.js');
 
-const coresNum = os.cpus(); // 16
-const { port1, port2 } = new MessageChannel();
+const coresNum = os.cpus();
+const num = 10;
 
 const performCalculations = async () => {
-    const worker = new Worker(pathToWorker);
-    worker.postMessage({ port: port2 }, [port2]);
-
-    port1.on('message', (message) => {
-        console.log('message from worker:', message);
-    });
-
-    // parentPort.on('message', () => {
-    //     const numberOfElements = 100;
-    //     const sharedBuffer = new SharedArrayBuffer(Int32Array.BYTES_PER_ELEMENT * numberOfElements);
-    //     const arr = new Int32Array(sharedBuffer);
-
-    //     for (let i = 0; i < numberOfElements; i += 1) {
-    //         arr[i] = Math.round(Math.random() * 30);
-    //     }
-
-    //     parentPort.postMessage({
-    //         arr
-    //     });
-    // });
+    const res = await Promise.all(new Array(coresNum.length).fill(null).map(async (_, i) => {
+        return new Promise((resovle, reject) => {
+            const worker = new Worker(pathToWorker);
+            worker.postMessage(num + i);
+            worker.on('message', (data) => {
+                worker.terminate();
+                const res = {
+                    status: 'resolved',
+                    data: data
+                };
+                resovle(res);
+            });
+            worker.on('error', (err) => {
+                const res = {
+                    status: 'error',
+                    data: null,
+                    errorMessage: err.message
+                };
+                reject(res);
+            })
+        });
+    }));
+    console.log('Result is', res);
 };
+
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,16 +1,15 @@
 import { parentPort } from 'worker_threads';
 
 const nthFibonacci = (n = 1) => {
-    const res = n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
-    return res;
+    return n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 };
 
 const sendResult = () => {
     parentPort.on('message', (data) => {
         const res = nthFibonacci(data);
         parentPort.postMessage(res);
+        process.exit();
     });
-    console.log(res);
 };
 
 sendResult();
