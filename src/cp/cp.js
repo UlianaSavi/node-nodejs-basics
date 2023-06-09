b2a6f2ac07e4b4fb6e0eabf105f1cb9d81c2f5f2
UlianaSavi <uljasha.sav@gmail.com>
2023-06-09 18:36:38 +0600

    feat: implement cp.js

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -4,15 +4,16 @@ import { spawn } from 'child_process';
 const __dirname = new URL('.', import.meta.url).pathname.slice(1);
 const filename = path.join(__dirname, 'files/script.js');
 
-const args = ['CLOSE', '2', '3', '4', '5', '6'];
+const args = ['here', 'my', 'args'];
 
 const spawnChildProcess = async (args) => {
-    const child = spawn('node', [filename]);
+    const child = spawn('node', [filename, args]);
+
     process.stdin.on('data', (chunk) => {
         child.stdin.write(chunk);
     });
-    child.stdout.on('data', (data) => {
-        console.log(data.toString());
+    child.stdout.on('data', (chunk) => {
+        console.log('Child message in master: ', chunk.toString());
     });
 
     child.on('close', (code) => {
@@ -22,10 +23,6 @@ const spawnChildProcess = async (args) => {
     child.on('error', (error) => {
         console.log(`child process exited with error ${error}`);
     });
-    process.stdin.on('close', (code) => {
-        console.log(`main process exited with code ${code}`);
-    });
 };
 
-// Put your arguments in function call to test this functionality
 spawnChildProcess(args);
